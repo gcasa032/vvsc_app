df133a728586b4b550b3ce77c17bb5012f3ce5a2
Guillermo Casal <gcasa032@fiu.edu>
2021-01-12 18:42:50 -0500

    Finished about section

--- vvsc_web/src/components/AboutMe/AboutMe.js
+++ vvsc_web/src/components/AboutMe/AboutMe.js
@@ -1,10 +1,35 @@
-import Jumbotron from "react-bootstrap/Jumbotron";
+import React from 'react';
+import Image from 'react-bootstrap/Image';
+import AboutText from './AboutText';
+import './AboutMe.css';
+
+import logo from '../../assets/img/AboutMe/logo.png';
 
 const aboutMe = () => {
+
+    const hrStyle = {
+        maxWidth: "50px",
+        borderWidth: "3px",
+        borderColor:" #fff"
+    }
+
     return (
-        <Jumbotron fluid={true}>
-            <h1>Hello World</h1>
-        </Jumbotron>
+        <div id='AboutMe' className="bg-danger" stlye={{background: 'linear-gradient(rgba(162,46,136,0.25), rgba(3,53,227,0.25))'}}>
+            <div className='container'>
+                <div className='row'>
+                    <div className='col'>
+                        <h2 className='text-white section-heading'>About VVSC</h2>
+                    </div>
+                </div>
+                <div className='row'>
+                    <div className='col offset-lg-8 mx-auto text-center'>
+                        <hr style={hrStyle}className='light my-4'/>
+                        <AboutText/>
+                        <Image style={{width: '150px'}} src={logo}/>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
 
---
+++ vvsc_web/src/components/AboutMe/AboutText.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './AboutMe.css';
+
+const aboutText = () => {
+    return (
+        <p class="text-faded mb-4">
+            <br />Vincent started competing in Powerlifting in 2014, which is also when he founded the Powerlifting team at 
+            Florida International University (FIU). Since then, he has coached <a href="https://www.instagram.com/fiupowerlifting/">
+            FIU Powerlifting</a> to 5 consecutive Florida collegiate state championship titles, something that has never been done before. 
+            Vince is the 2016 Nation Bench Press champion and currently still holds the Florida Junior Bench Press state record. 
+            Having competed and coached all over the US, including Rhode Island, Colorado, Atlanta, Ohio and Washington, 
+            he was also asked to help coach the United States Bench Press National team. He traveled to Finland for IPF World 
+            Championships in 2018 and assisted in coaching the US team on the international stage.  <br />In 2016 Vincent started his 
+            official coaching business, VVSC - which gives lifters of all experience levels the opportunity for 1-on-1 personalized coaching, 
+            at an affordable price. Vince customizes programming with any previous injuries or limitations in mind and will then be there every 
+            week to provide feedback, analyze lifts and adjust based on performance. Coaching also includes meet prep for any athlete that wishes 
+            to compete in Powerlifting. If it&#39;s your first time stepping on the platform, Vincent will guide you through the signup process
+             and prepare you for meet day.<br />Vincent has a Bachelors in Physical Education from Florida International University and is an 
+             officially certified USAPL (USA Powerlifting) Coach. Vince is also a licensed Emergency Medical Technician (EMT) and is certified
+              in Basic Life Support (BLS) by the American Heart Association. <br /></p>
+    )
+}
+
+export default aboutText;
